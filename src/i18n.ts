8f0b36746fe20b3bbba7b2a7cb79f17ab3adc11b
yuliiakucher <yuliia.s.kucher@gmail.com>
2026-07-02 23:55:56 +0300

    fix: updated i18next config

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,19 +4,17 @@ import Backend from 'i18next-http-backend';
 
 
 i18n
-  .use(initReactI18next)// passes i18n down to react-i18next
   .use(Backend)
+  .use(initReactI18next)// passes i18n down to react-i18next
   .init({
-    lng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
-
+    lng: 'en',
+    ns: ['nav'],
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
     backend: {
       // for all available options read the backend's repository readme file
-      loadPath: 'lang/en/nav.json',
+      loadPath: '/lang/{{lng}}/{{ns}}.json',
     },
   });
 
